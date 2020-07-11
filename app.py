95ed990b5ca2693bea651dc9b6c10c904ac1de80
Максим Кузнецов <m.kuznetsov@restream.rt.ru>
2020-07-12 02:14:02 +0300

    Проектное задание главы 3

---
+++ app.py
@@ -0,0 +1,85 @@
+import random
+from flask import Flask, render_template, request
+import prepare_script
+# Инициализировать начальное состояние базы данных
+prepare_script.create_db(rewrite_if_db_exists=False)
+import forms
+import db_manager
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    _teachers_list = db_manager.get_all_teachers()
+    random.shuffle(_teachers_list)
+    return render_template(
+        "index.html",
+        goals=db_manager.get_all_goals(),
+        teachers=_teachers_list[:6]
+    )
+
+
+@app.route('/goals/<goal>/')
+def goals(goal):
+    return render_template(
+        "goal.html",
+        goal=db_manager.get_all_goals(filter_by_key=goal)[0],
+        teachers=db_manager.get_all_teachers(filter_by_goal=goal)
+    )
+
+
+@app.route('/profiles/<int:teacher_id>/')
+def profile(teacher_id):
+    return render_template(
+        "profile.html",
+        teacher=db_manager.get_all_teachers(filter_by_id=teacher_id)[0]
+    )
+
+
+@app.route('/request/', methods=['POST', 'GET'])
+def teacher_request():
+    form = forms.RequestForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            db_manager.insert_request(**form.data)
+            return render_template('request_done.html', form=form)
+
+    return render_template('request.html', form=form)
+
+
+@app.route('/booking/<int:teacher_id>/<weekday>/<time>', methods=["POST", "GET"])
+def booking(teacher_id, weekday, time):
+    form = forms.BookingForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            db_manager.insert_booking(**form.data)
+            return render_template("booking_done.html", form=form)
+
+    return render_template(
+        "booking.html",
+        teacher=db_manager.get_all_teachers(filter_by_id=teacher_id)[0],
+        weekday=weekday,
+        time=time,
+        form=form
+    )
+
+
+@app.template_filter('as_rus_weekday')
+def as_rus_weekday(day):
+    """ Вернуть день недели """
+    return {'mon': 'Понедельник', 'tue': 'Вторник', 'wed': 'Среда',
+            'thu': 'Четверг', 'fri': 'Пятница', 'sat': 'Суббота',
+            'sun': 'Воскресенье'}[day]
+
+
+@app.template_filter('as_goal_title')
+def as_goal_title(goal):
+    """ Вернуть название переданной цели """
+    return db_manager.get_all_goals(filter_by_key=goal)[0]['name']
+
+
+if __name__ == '__main__':
+    app.secret_key = 'my-super-secret-phrase-I-dont-tell-this-to-nobody'
+    app.run()
---
+++ forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, RadioField, SubmitField, HiddenField
+from wtforms.validators import Length
+import db_manager
+
+
+class RequestForm(FlaskForm):
+    title = StringField('Подбор преподавателя')
+    subtitle = StringField('Напишите, чего вам нужно и мы подберем отличных ребят')
+
+    goals = RadioField(
+        'Какая цель занятий?',
+        choices=[(i['key'], i['name']) for i in db_manager.get_all_goals()],
+        default="travel")
+
+    free_times = RadioField('Сколько времени есть?', choices=[
+        ("1-2 часа в неделю", "1-2 часа в неделю"),
+        ("3-5 часов в неделю", "3-5 часов в неделю"),
+        ("5-7 часов в неделю", "5-7 часов в неделю"),
+        ("7-10 часов в неделю", "7-10 часов в неделю")
+    ], default="3-5 часов в неделю")
+
+    name = StringField("Вас зовут", [Length(min=1, message="Имя должно содержать не менее одного символа")])
+    phone = StringField("Ваш телефон", [Length(min=7, message="Номер телефона должен содержать не менее 7и символов")])
+    submit = SubmitField('Найдите мне преподавателя')
+
+
+class BookingForm(FlaskForm):
+    name = StringField("Вас зовут", [Length(min=1, max=50, message="Имя должно содержать не менее одного символа")])
+    phone = StringField("Ваш телефон", [Length(min=7, max=30, message="Номер телефона должен содержать не менее 7и символов")])
+
+    weekday = HiddenField()
+    time = HiddenField()
+    teacher_id = HiddenField()
+
+    submit = SubmitField('Записаться на пробный урок')
